c094461aca2f212422070f3bc49d3e2bcf5c94a6
fazru siregar <fazrumuwazirsiregar@gmail.com>
2018-11-20 20:16:30 +0700

    data master

---
+++ konek.php
@@ -0,0 +1,8 @@
+<?php
+$server="localhost";
+$id="fazrusiregar";
+$password="fazrusiregar02";
+$database="dblogin";
+$mysqli=mysqli_connect($server,$id,$password,$database) or die ("connect fail");
+
+?>
